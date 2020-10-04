a0b330d6c16116366f35c9ceee125c5424a5d27c
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2020-10-04 15:29:28 +0500

    screen 2 finished

--- astro-irina/js/script.js
+++ astro-irina/js/script.js
@@ -20,8 +20,19 @@ function testWebP(callback) {
 $(document).ready(function() {
 
     $('.services__slider').slick({
-      slidesToShow: 1
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: false,
+      variableWidth: true
   });
+
+});
+let $status = $('.pagingInfo');
+let $slickElement = $('.services__slider');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  let i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '/' + slick.slideCount);
 });
 $(document).ready(function() {
 
--- src/js/slick-slider.js
+++ src/js/slick-slider.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
     $('.services__slider').slick({
-      slidesToShow: 1
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: false,
+      variableWidth: true
   });
+
+});
+let $status = $('.pagingInfo');
+let $slickElement = $('.services__slider');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  let i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '/' + slick.slideCount);
 });
