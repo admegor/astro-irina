f4d255f797e9749b1cf3ddf4826f9b24f0061e7f
admegor <admegor@mail.ru>
2020-10-05 17:08:15 +0500

    screen 1 mobile 50%

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,10 @@
 let menuBtn = document.querySelector('.header__toggle-wrapper');
 let menuBurger = document.querySelector('.header__toggle');
+let menuMobile = document.querySelector('.menu__mobile');
+let header = document.querySelector('.header');
 
 menuBtn.onclick = function() {
     menuBurger.classList.toggle('active');
+    menuMobile.classList.toggle('active');
+    header.classList.toggle('open');
 }
