30b7a8f7d6684b0feaaebbacc5428ec7ceb6cae7
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2020-10-11 22:33:55 +0500

    screen 7 50%

--- astro-irina/js/script.js
+++ astro-irina/js/script.js
@@ -19,6 +19,16 @@ function testWebP(callback) {
 
 $(document).ready(function() {
 
+//   $('.faq__accordion-title').click(function() {
+//     $(this).next('.faq__accordion-inner').slideToggle();
+// })
+
+$('.faq__accordion').accordion({
+  // heightStyle: 'content',
+  collapsible: true,
+	header: '> .faq__accordion-item > .faq__accordion-title'
+});
+
     $('.services__slider').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -26,13 +36,39 @@ $(document).ready(function() {
       variableWidth: true
   });
 
+    $('.feedback__slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: false,
+      variableWidth: true,
+      responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            variableWidth: false
+          }
+        }
+      ]
+  });
+
 });
+
+
+
 let $status = $('.pagingInfo');
 let $slickElement = $('.services__slider');
 
 $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
   let i = (currentSlide ? currentSlide : 0) + 1;
   $status.text(i + '/' + slick.slideCount);
+});
+
+let $statusFB = $('.pagingInfoFeedback');
+let $slickElementFB = $('.feedback__slider');
+
+$slickElementFB.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  let i = (currentSlide ? currentSlide : 0) + 1;
+  $statusFB.text(i + '/' + slick.slideCount);
 });
 $(document).ready(function() {
 
